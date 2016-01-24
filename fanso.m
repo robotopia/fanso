cc65a47c42eba19a79111c33b1e841cfd83d4b3e
Sam McSweeney <sammy.mcsweeney@gmail.com>
2016-01-24 14:23:03 +0800

    Can now display FFTs of the visible part of the timeseries

--- fanso.m
+++ fanso.m
@@ -1,4 +1,15 @@
 function fanso(timeseries, dt)
+% -- Function: fanso (timeseries, dt)
+%     Analyse a timeseries using the Fourier ANalysis Suite for Octave.
+%     Requires FLTK.
+%
+%     timeseries = a column vector of real values.
+%
+%     dt = a scalar (representing the time between consecutive
+%          points in timeseries).
+%
+%     Written by Sam McSweeney, 2016, Creative Commons Licence
+%     sammy.mcsweeney@gmail.com
 
 % Calculate default dimensions of figure windows
 global screensize = get(0,'screensize');
@@ -8,12 +19,11 @@ global defaultheight = floor((screensize(4) - 3*gapsize)/2);
 global fig_x = [gapsize, gapsize];
 global fig_y = [2*gapsize + defaultheight, gapsize];
 
-
 % Require that we are using the correct graphics toolkit
 gtk = 'fltk';
 if (~any(strcmp(available_graphics_toolkits(), gtk)))
-  disp('This program requires ',gtk,' to be installed.')
-  exit
+  disp('This function requires ',gtk,' to be installed.')
+  return;
 else
   graphics_toolkit(gtk);
 end % if
@@ -31,36 +41,30 @@ plot(t, timeseries);
 xlabel('Time (s)');
 ylabel('Timeseries values');
 
-m = uimenu("label", "FF&T", "accelerator", "F", "callback", {@plot_fft, "timeseries"});
+m = uimenu("label", "FF&T", "accelerator", "F", "callback", {@plot_fft, dt, timeseries});
+%plot_fft(timeseries)
 
 end % function
 
 
-% Usage function
-function my_usage()
-  disp('usage: fanso FILENAME')
-  disp('  FILENAME must be an Octave-produced ASCII file containing')
-  disp('  the variables ''dt'', a scalar representing the time between')
-  disp('  consecutive data points (in seconds), and ''timeseries'', the')
-  disp('  values of the timeseries data points.')
-  exit
-end % function
-
-% Display online help -- Prefer to user menus and accelerators. Delete me when this function is no longer used.
-function online_help()
-  disp('b\tToggle breakpoints mode (init = off). When on, left mouse button')
-  disp('\tclicks in the timeseries window will introduce a breakpoint. Right')
-  disp('\tclicks will remove the nearest breakpoint.')
-  disp('h\tShows this online help.')
-end % function
-
-function plot_fft(timeseries)
-timeseries
+function plot_fft(src,data,dt,timeseries)
+% This is a callback function
+  % Set position,size of figure 2 window
   global fig_x
   global fig_y
   global defaultwidth
   global defaultheight
   figure(2, "Position", [fig_x(2), fig_y(2), defaultwidth, defaultheight]);
+
+  % Get only portion of timeseries visible in Figure 1
+  figure(1);
+  a = axis();
+  min_idx = max([floor(a(1)/dt)+1, 1]);
+  max_idx = min([floor(a(2)/dt)+1, length(timeseries)]); % <-- Check this
+  timeseries = timeseries([min_idx:max_idx]);
+
+  % Plot it up
+  figure(2);
   ffted = fft(timeseries);
   absed = abs(ffted);
   plot(absed);
